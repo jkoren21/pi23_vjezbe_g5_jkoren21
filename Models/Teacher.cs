e1ec34c87225eef1f927e7bb7d974dd3101cb0b2
pi <programskoinjzinjerstvo@foi.hr>
2023-05-15 18:34:19 +0200

    prijava povezana s bazom

--- Models/Teacher.cs
+++ Models/Teacher.cs
@@ -8,6 +8,10 @@ namespace Evaluation_Manager.Models {
     public class Teacher : Person{
         public string Username { get; set; }
         public string Password { get; set; }
+
+        public bool CheckPassword(string password) {
+            return Password == password;
+        }
     }
 
 }
