eb7efbfaf616f67f105e98712411874697486f5b
pi <programskoinjzinjerstvo@foi.hr>
2023-05-22 18:26:44 +0200

    implementacija metoda klasa student

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,13 +1,36 @@
-﻿using System;
+﻿using Evaluation_Manager.Repositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Evaluation_Manager.Models
-{
-    public class Student : Person
-    {
+namespace Evaluation_Manager.Models {
+    public class Student : Person {
         public int Grade { get; set; }
     }
+
+    public List<Evaluation> GetEvaluations() {
+        return EvaluationRepository.GetEvaluations(this);
+    }
+    public int CalculateTotalPoints() {
+        int totalPoints = 0;
+        foreach(var evaluation in GetEvaluations()) {
+            totalPoints += evaluation.Points;
+        }
+        return totalPoints;
+    }
+
+    public bool HasSignature() {
+        bool hasSignature = true;
+        if(IsEvaluationComplete() == true) {
+            foreach(var evaluation in GetEvaluations()) {
+                if(evaluation.IsSufficient)
+            }
+        }
+    }
+   
+        
+        
+    
 }
