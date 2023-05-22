0f25ed773cbdfc4504a6db0323eecc63a68b20d0
pi <programskoinjzinjerstvo@foi.hr>
2023-05-22 19:10:51 +0200

    kod radi

--- Models/Student.cs
+++ Models/Student.cs
@@ -8,28 +8,76 @@ using System.Threading.Tasks;
 namespace Evaluation_Manager.Models {
     public class Student : Person {
         public int Grade { get; set; }
-        public string HasSignature { get; internal set; }
-    }
 
-    public List<Evaluation> GetEvaluations() {
-        return EvaluationRepository.GetEvaluations(this);
-    }
-    public int CalculateTotalPoints() {
-        int totalPoints = 0;
-        foreach(var evaluation in GetEvaluations()) {
-            totalPoints += evaluation.Points;
+        public List<Evaluation> GetEvaluations() {
+            return EvaluationRepository.GetEvaluations(this);
         }
-        return totalPoints;
-    }
+        public int CalculateTotalPoints() {
+            int totalPoints = 0;
+            foreach (var evaluation in GetEvaluations()) {
+                totalPoints += evaluation.Points;
+            }
+            return totalPoints;
+        }
+        private bool isEvaluationComplete() {
+            var evaluations = GetEvaluations();
+            var activities = ActivityRepository.GetActivities();
 
-    public bool HasSignature() {
-        bool hasSignature = true;
-        if(IsEvaluationComplete() == true) {
-            foreach(var evaluation in GetEvaluations()) {
-                if (evaluation.IsSufficient) ;
+            return evaluations.Count == activities.Count;
+        }
+
+        public bool HasSignature() {
+            bool hasSignature = true;
+            if (IsEvaluationComplete() == true) {
+                foreach (var evaluation in GetEvaluations()) {
+                    if (evaluation.IsSufficientForSignature() == false) {
+                        hasSignature = false;
+                        break;
+                    }
+                }
+            } else {
+                hasSignature=false;
+            }
+            return = hasSignature;
+        }
+
+        public bool HasGrade() {
+            bool hasGrade = true;
+            if (isEvaluationComplete)){
+                foreach(var evaluation in GetEvaluations()) {
+                    if(evaluation.IsSufficentForGrade() == false) {
+                        hasGrade = false;
+                        break;
+                    }
+                }
+            }
+            else {
+                hasGrade = false;
             }
         }
-    }
+
+        public int CalculateGrade() {
+            int grade = 0;
+            if (HasGrade()) {
+                int totalPoints = CalculateTotalPoints();
+                if (totalPoints >= 91) {
+                    grade = 5;
+                }
+                else if (totalPoints >= 76) {
+                    grade = 4;
+                }
+                else if(totalPoints >= 61) {
+                    grade = 3;,
+                }
+                else if(totalPoints >= 50) {
+                    grade = 2;
+                }
+                else {
+                    grade = 1;
+                }
+            }
+            return grade;
+        }
    
         
         
